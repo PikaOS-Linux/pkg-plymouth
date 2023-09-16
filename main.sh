87684c81d3c23177f0e9b45ef3dbb6953f1d41f6
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-16 13:07:02 +0300

    Add dynamic branding

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
+# Get Logos
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-simple-small.svg -O ./plymouth/debian/local/bgrt-fallback.png
+wget https://github.com/PikaOS-Linux/pika-branding/raw/main/logos/pika-logo-text-dark.svg -O ./plymouth/debian/local/pika-logo.png
 # Clone Upstream
 cd ./plymouth
 
@@ -5,7 +8,7 @@ cd ./plymouth
 apt-get build-dep ./ -y
 
 # Build package
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
